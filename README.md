f1481f3516c9bbddbf21ae31ce8ca7a52ab354db
mildmojo <theaterfire@yahoo.com>
2014-01-31 01:57:45 -0500

    Initial version. Needs testing.

--- README.md
+++ README.md
@@ -1,4 +1,44 @@
 twine-gamepad
 =============
 
-Twine add-on that provides gamepad support.
+This is a Twine extension that lets you play your game with a gamepad. It uses
+the HTML5 Gamepad API, which is supported in some versions of Firefox and
+Chrome. Use the analog stick and D-pad to select links and press any face button
+to visit them.
+
+See the `example.tws` story file for a real-world usage example.
+
+## Usage
+
+1. Edit `twine_gamepad.twee`.
+2. Make sure paths to `gamepad.min.js` and `twine_gamepad.js` look correct
+   relative to your story's final export path.
+3. Modify the "selected link" CSS style if desired.
+4. Add `twine_gamepad.twee` to your story's StoryIncludes passage.
+5. Build your story.
+6. Package your story or deploy it to a website along with the `gamepad.min.js`
+   and `twine_gamepad.js` files.
+
+You may also specify the "selected link" CSS style in your main story file's
+stylesheet. If you do, you may need to use `!important` to override the
+library's defaults.
+
+```css
+a.gamepadSelected {
+  border: 2px dotted white !important;
+}
+```
+
+## Alternate single-file usage
+
+Alternately, you may copy the contents of `gamepad.min.js` and
+`twine_gamepad.js` directly into a single `script` passage in your story. Make
+sure `gamepad.min.js` comes before `twine_gamepad.js`. Make sure to add a CSS
+rule to your stylesheet for `a.gamepadSelected` to make it appear selected.
+
+Maybe the separate-file method is bonkers and this is the way it should be by
+default in `twine_gamepad.twee`.
+
+## Thanks
+
+Thanks to @kallaspriit for the excellent [HTML5-JavaScript-Gamepad-Controller-Library](https://github.com/kallaspriit/HTML5-JavaScript-Gamepad-Controller-Library).
---
+++ twine_gamepad.js
@@ -0,0 +1,106 @@
+(function(exports) {
+  var Gamepad;
+  if ((typeof(module) !== 'undefined') && module.exports) {
+    Gamepad = require('gamepad');
+  } else {
+    Gamepad = window.Gamepad;
+  }
+  var gamepad = new Gamepad();
+  var axes = { LEFT_STICK_X: 0, LEFT_STICK_Y: 0 };
+  var DEADZONE = 0.5;
+
+  gamepad.bind(Gamepad.Event.CONNECTED, function(device) {
+    console.log('Gamepad connected.');
+  });
+
+  gamepad.bind(Gamepad.Event.DISCONNECTED, function(device) {
+    console.log('Gamepad disconnected.');
+  });
+
+  gamepad.bind(Gamepad.Event.BUTTON_DOWN, function(e) {
+    switch(e.control) {
+      case 'DPAD_DOWN':
+      case 'DPAD_RIGHT':
+        nextLink();
+        break;
+      case 'DPAD_UP':
+      case 'DPAD_LEFT':
+        prevLink();
+        break;
+      case 'FACE_1':
+      case 'FACE_2':
+      case 'FACE_3':
+      case 'FACE_4':
+        jQuery('a.internalLink.gamepadSelected').click();
+        break;
+    }
+  });
+
+  // Treat axes as buttons; register presses and releases.
+  gamepad.bind(Gamepad.Event.AXIS_CHANGED, function(data) {
+    var newValue = 0;
+    if (Math.abs(data.value) > DEADZONE) {
+      newValue = data.value < 0 ? -1 : 1;
+      if (axes[data.axis] !== newValue) {
+        if (data.axis === 'LEFT_STICK_X') {
+          if (newValue === -1) {
+            gamepad._fire(Gamepad.Event.BUTTON_DOWN, {control: 'DPAD_LEFT'});
+          } else {
+            gamepad._fire(Gamepad.Event.BUTTON_DOWN, {control: 'DPAD_RIGHT'});
+          }
+        } else if (data.axis === 'LEFT_STICK_Y') {
+          if (newValue === 1) {
+            gamepad._fire(Gamepad.Event.BUTTON_DOWN, {control: 'DPAD_DOWN'});
+          } else {
+            gamepad._fire(Gamepad.Event.BUTTON_DOWN, {control: 'DPAD_UP'});
+          }
+        }
+      }
+    }
+    axes[data.axis] = newValue;
+  });
+
+  gamepad.init();
+
+  function nextLink() {
+    var links = jQuery('div.passage.body a');
+    var newIndex = 0;
+    var $first = jQuery(links[0]);
+    var $last = jQuery(links[links.length - 1]);
+    if (links.is('.gamepadSelected') && ! $last.is('.gamepadSelected')) {
+      links.each(function(_idx, el) {
+        var $el = jQuery(el);
+        if ($el.is('.gamepadSelected')) {
+          $el.removeClass('gamepadSelected');
+          newIndex = _idx + 1;
+        }
+      });
+    } else if ($last.is('.gamepadSelected')) {
+      $last.removeClass('gamepadSelected');
+    }
+    jQuery(links[newIndex]).addClass('gamepadSelected');
+  }
+
+  function prevLink() {
+    var links = jQuery('div.passage.body a');
+    var newIndex = 0;
+    var $first = jQuery(links[0]);
+    var $last = jQuery(links[links.length - 1]);
+    if (links.is('.gamepadSelected')) {
+      if ($first.is('.gamepadSelected')) {
+        $first.removeClass('gamepadSelected');
+        newIndex = links.length - 1;
+      } else {
+        links.each(function(_idx, el) {
+          var $el = jQuery(el);
+          if ($el.is('.gamepadSelected')) {
+            $el.removeClass('gamepadSelected');
+            newIndex = _idx - 1;
+          }
+        });
+      }
+    }
+    jQuery(links[newIndex]).addClass('gamepadSelected');
+  }
+
+})(((typeof(module) !== 'undefined') && module.exports) || window);
